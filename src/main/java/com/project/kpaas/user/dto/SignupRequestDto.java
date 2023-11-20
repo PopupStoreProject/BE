15148a5f4c157d15da89d294819b27094a69c22e
YeowonKIM <kkyoo152@gmail.com>
2023-11-20 20:08:40 +0900

    Feat : 일반 사용자 마이페이지 #17

--- src/main/java/com/project/kpaas/user/dto/SignupRequestDto.java
+++ src/main/java/com/project/kpaas/user/dto/SignupRequestDto.java
@@ -2,10 +2,13 @@ package com.project.kpaas.user.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.lang.Nullable;
 
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
+import java.util.List;
 
 @Getter @Setter
 public class SignupRequestDto {
@@ -20,6 +23,9 @@ public class SignupRequestDto {
     @NotNull(message = "비밀번호는 필수 값입니다.")
     @Pattern(regexp = "^[a-zA-Z0-9\\d`~!@#$%^&*()-_=+]{8,24}$", message = "비밀번호는 대소문자 및 특수문자 포함 8-24자 이내 입니다.")
     private String password;
+
+    @Nullable
+    private List<String> categoryPreference;
 
     private boolean admin = false;
     private String adminToken = "";
