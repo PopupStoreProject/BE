7a0b7d559ba569f03d4f897c90ac6077a94f8266
YeowonKIM <kkyoo152@gmail.com>
2023-12-01 12:45:48 +0900

    Feat : 공간 데이터 반경 조회 기능 #31

--- src/main/java/com/project/kpaas/popup/controller/PopupController.java
+++ src/main/java/com/project/kpaas/popup/controller/PopupController.java
@@ -50,4 +50,12 @@ public class PopupController {
         return popupService.deletePopup(id, userDetails.getUser());
     }
 
+    // radius는 meter단위(ex. 1000 = 1000m = 1km)
+    @GetMapping("/gps")
+    public ResponseEntity<List<PopupResponseDto>> gpsSearch(@RequestParam(value = "lat") double lat,
+                                                            @RequestParam(value = "lon") double lon,
+                                                            @RequestParam(value = "radius") double radius) {
+        return popupService.searchByRadius(lat, lon, radius);
+    }
+
 }
